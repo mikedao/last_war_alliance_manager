e7791e53550daf0c96c2d390a5ee5e45169031aa
Michael Dao <mike@turing.edu>
2025-06-23 19:30:59 -0400

    Remove Tailwind CSS initializer as part of configuration cleanup

--- config/initializers/tailwindcss.rb
+++
@@ -1,9 +0,0 @@
-Tailwindcss::Rails.configure do |config|
-  config.config_path = Rails.root.join("config", "tailwind.config.js")
-  config.content = [
-    Rails.root.join("app/views/**/*.{html,html.erb,erb,slim}"),
-    Rails.root.join("app/helpers/**/*.rb"),
-    Rails.root.join("app/javascript/**/*.js"),
-    Rails.root.join("public/*.html"),
-  ]
-end
